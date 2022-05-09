8f105fe4c289b293bec7319d94f92997293f06fd
lucietti <77293908+LuciaFores@users.noreply.github.com>
2022-05-09 13:01:29 +0200

    Inizio popolamento array abilità
    - È stato creato l'array delle abilità che lega un nome ad una immagine
    - È stato testato il suo funzionamento nel riuscire a prendere nome e immagine e disporli in una pagina web

---
+++ src/components/Abilities.js
@@ -0,0 +1,25 @@
+import imgCane from '../img/abilities_img/imgCane.svg';
+import imgGatto from '../img/abilities_img/imgGatto.svg';
+import imgElefante from '../img/abilities_img/imgElefante.svg';
+import imgCavallo from '../img/abilities_img/imgCavallo.svg';
+
+let abilities = [
+    {
+        name : "Cane",
+        img : imgCane
+    },
+    {
+        name : "Gatto",
+        img : imgGatto
+    },
+    {
+        name : "Elefante",
+        img : imgElefante
+    },
+    {
+        name : "Cavallo",
+        img : imgCavallo
+    }
+]
+
+export default abilities;
